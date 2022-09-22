b0362833a28700ce709eee3b60d8bc6d2ade8cce
Ramez Taher <ramezgamer11@gmail.com>
2022-09-22 18:22:40 +0100

    header in progress

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,51 @@
 import React from "react"
+import { SocialIcon } from "react-social-icons"
 
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <header>
+      <div className="container sm:mx-auto px-4">
+        <div className="flex items-center">
+          <SocialIcon
+            url="https://twitter.com/jaketrent"
+            fgColor="gray"
+            bgColor="transparent"
+          />
+          <SocialIcon
+            url="https://twitter.com/jaketrent"
+            fgColor="gray"
+            bgColor="transparent"
+          />
+          <SocialIcon
+            url="https://twitter.com/jaketrent"
+            fgColor="gray"
+            bgColor="transparent"
+          />
+          <SocialIcon
+            url="https://twitter.com/jaketrent"
+            fgColor="gray"
+            bgColor="transparent"
+          />
+          <SocialIcon
+            url="https://twitter.com/jaketrent"
+            fgColor="gray"
+            bgColor="transparent"
+          />
+        </div>
+        <div className="flex items-center cursor-pointer">
+          <SocialIcon
+            className="cursor-pointer"
+            network="email"
+            fgColor="gray"
+            bgColor="transparent"
+          />
+          <p className="uppercase hidden md:inline-flex text-sm text-gray">
+            Contact Me
+          </p>
+        </div>
+      </div>
+    </header>
+  )
 }
 
 export default Header
