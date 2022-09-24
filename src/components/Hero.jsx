229bf28028acf33f9effdb47f9b112a3d731559a
Ramez Taher <ramezgamer11@gmail.com>
2022-09-24 23:21:00 +0100

    Cleaning the Code

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,27 +10,25 @@ const Hero = () => {
   })
   return (
     <div className="h-screen flex flex-col gap-y-8 justify-center items-center text-center overflow-hidden ">
-      <div className="flex flex-col gap-8 justify-center items-center">
-        <img
-          className="relative rounded-full h-48 w-48 mx-auto object-cover"
-          src={ramez}
-          alt="Ramez"
-        />
-        <div className=" flex flex-col items-center gap-2">
-          <h2 className="text-md uppercase tracking-[15px] font-medium">
-            Software Engineer
-          </h2>
-          <h1 className="flex items-center text-2xl font-bold gap-3">
-            <p className="text-5xl lg:text-6xl font-semibold px-10">{text}</p>
-            <Cursor cursorColor="#f7abba" />
-          </h1>
+      <img
+        className="relative rounded-full h-48 w-48 mx-auto object-cover"
+        src={ramez}
+        alt="Ramez"
+      />
+      <div className=" flex flex-col items-center gap-6">
+        <h2 className="text-md uppercase tracking-[15px] font-medium">
+          Software Engineer
+        </h2>
+        <h1 className="flex items-center text-2xl font-bold gap-3">
+          <p className="text-5xl lg:text-6xl font-semibold px-10">{text}</p>
+          <Cursor cursorColor="#f7abba" />
+        </h1>
 
-          <div>
-            <button className="btn-hero">About</button>
-            <button className="btn-hero">Experience</button>
-            <button className="btn-hero">Skills</button>
-            <button className="btn-hero">Projects</button>
-          </div>
+        <div>
+          <button className="btn-hero">About</button>
+          <button className="btn-hero">Experience</button>
+          <button className="btn-hero">Skills</button>
+          <button className="btn-hero">Projects</button>
         </div>
       </div>
     </div>
