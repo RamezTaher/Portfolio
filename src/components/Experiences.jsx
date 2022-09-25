4d319edb6e04f1ee922f4643ee6149c1a43ff0e9
Ramez Taher <ramezgamer11@gmail.com>
2022-09-25 15:54:03 +0100

    Working on experiences section

--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -1,7 +1,13 @@
 import React from "react"
 
 const Experiences = () => {
-  return <div>Experiences</div>
+  return (
+    <div className="h-screen flex flex-col text-left md:flex-row justify-evenly mx-auto items-center relative py-10 overflow-hidden">
+      <h3 className="absolute top-8 uppercase tracking-[20px] text-2xl text-gray">
+        Experiences
+      </h3>
+    </div>
+  )
 }
 
 export default Experiences
