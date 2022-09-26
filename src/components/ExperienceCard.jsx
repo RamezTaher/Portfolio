cdf3a8da7dfeee183e843fcbffc65a33d133e296
Ramez Taher <ramezgamer11@gmail.com>
2022-09-26 21:20:58 +0100

    Fixing seo

--- src/components/ExperienceCard.jsx
+++ src/components/ExperienceCard.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react"
 
 const ExperienceCard = () => {
-  return (
-    <div>
-      <h3 className="absolute top-24 uppercase trancking-[20px] text-gray text-2xl">Experience</h3>
-    </div>
-  )
+  return <article></article>
 }
 
 export default ExperienceCard
