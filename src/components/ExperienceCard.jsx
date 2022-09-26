d38fea70edf768a76786a7c2a4c621101f061a76
Ramez Taher <ramezgamer11@gmail.com>
2022-09-26 21:24:30 +0100

    building the cards component

--- src/components/ExperienceCard.jsx
+++ src/components/ExperienceCard.jsx
@@ -1,7 +1,17 @@
 import React from "react"
+import { motion } from "framer-motion"
+import ramez from "../assets/images/ram-1.jpg"
 
 const ExperienceCard = () => {
-  return <article></article>
+  return (
+    <article>
+      <motion.img
+        src={ramez}
+        className="w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center"
+        alt="Company Name"
+      />
+    </article>
+  )
 }
 
 export default ExperienceCard
