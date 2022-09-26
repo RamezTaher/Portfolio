fa8560ce4a00d97e17646a7a650cee5161c05558
Ramez Taher <ramezgamer11@gmail.com>
2022-09-26 21:29:40 +0100

    Cards in progress

--- src/components/ExperienceCard.jsx
+++ src/components/ExperienceCard.jsx
@@ -6,10 +6,34 @@ const ExperienceCard = () => {
   return (
     <article>
       <motion.img
+        initial={{
+          y: -100,
+          opacity: 0,
+        }}
+        transition={{
+          duration: 1.2,
+        }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
         src={ramez}
         className="w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center"
         alt="Company Name"
       />
+
+      <div className="px-0 md:px-10 flex flex-col gap-2">
+        <h4 className="text-4xl font-light">Fullstack Developer</h4>
+        <p className="font-bold text-2xl">Ramez's Company</p>
+        <div className="flex items-center gap-2 ">{/* stack */}</div>
+        <p>Started work ... ended work ...</p>
+
+        <ul className="list-disc flex flex-col gap-4 ml-5 text-lg">
+          <li>Summary points</li>
+          <li>Summary points</li>
+          <li>Summary points</li>
+          <li>Summary points</li>
+          <li>Summary points</li>
+        </ul>
+      </div>
     </article>
   )
 }
