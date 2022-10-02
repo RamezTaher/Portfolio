e5d64f398350943db948c7a054f217d060a99e20
Ramez Taher <ramezgamer11@gmail.com>
2022-10-02 15:46:00 +0100

    technologies section finished

--- src/components/Technologie.jsx
+++ src/components/Technologie.jsx
@@ -4,12 +4,13 @@ import ramez from "../assets/images/ram-1.jpg"
 
 const Technologie = ({ isAnimatedToLeft }) => {
   return (
-    <div className="group relative cursor-pointer">
+    <div className="group cursor-pointer">
       <motion.img
         initial={{ x: isAnimatedToLeft ? -200 : 200, opacity: 0 }}
         transition={{ duration: 1 }}
         whileInView={{ x: 0, opacity: 1 }}
         src={ramez}
+        className="rounded-full border border-gray border-solid object-cover object-center w-24 h-24 lg:h-32 lg:w-32 filter grayscale group-hover:grayscale-0 transition duration-300 ease-in-out"
       />
     </div>
   )
